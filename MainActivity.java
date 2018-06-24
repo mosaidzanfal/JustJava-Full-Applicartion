db0926acf09ce46d38605fe281a0a02b7d497ef4
Mosaid Zeno <mosaad7@gmail.com>
2018-06-24 20:04:17 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,55 +1,70 @@
-/**
- * IMPORTANT: Make sure you are using the correct package name.
- * This example uses the package name:
- * package com.example.android.justjava
- * If you get an error when copying this code into Android studio, update it to match teh package name found
- * in the project's AndroidManifest.xml file.
- **/
-
+@@ -9,12 +9,19 @@
 package com.example.mosaidzeno.justjava;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-import java.text.NumberFormat;
-
-/**
+@ -24,14 +31,23 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int quantity ;
+    int price;
+    boolean hasWhippedCream;
+    boolean hasChecolat;
+    CheckBox hasWC ;
+    CheckBox HasCheco ;
+    String OrderSummary;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     int quantity = 1;
     int price;
+
+
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
-
-        createOrderSummary();
-    }
-
-    /**
-     * Calculates the price of the order.
-     *
+@ -46,54 +62,94 @@ public class MainActivity extends AppCompatActivity {
      * @param quantity is the number of cups of coffee ordered
      */
     private int calculatePrice(int quantity,int xprice) {
+        hasWC = findViewById(R.id.Haswhippedcream);
+        HasCheco = findViewById(R.id.chocolate_checkbox);
+        hasWhippedCream = hasWC.isChecked();
+        hasChecolat = HasCheco.isChecked();
+
+        if (hasWhippedCream){
+            xprice = xprice + 1;
+        }
+
+        if ((hasChecolat)){
+            xprice = xprice + 2;
+        }
         price = quantity * xprice;
         return  price;
     }
     public void display(int number) {
+    public void displayQuantity(int number) {
 
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
@@ -65,6 +80,11 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public  void createOrderSummary(){
+        hasWC = findViewById(R.id.Haswhippedcream);
+        HasCheco = findViewById(R.id.chocolate_checkbox);
+        hasWhippedCream = hasWC.isChecked();
+        hasChecolat = HasCheco.isChecked();
+
         TextView priceTextView = findViewById(R.id.orderSummaryTextView);
         String CName = "Customer Name";
         display(quantity);
@@ -72,6 +92,25 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(CName + "\n" +"quantity : " + quantity +
                                       "\n" + "Total is : " + price +
                                       "\nThanks You !! ");
+        EditText UserName  = findViewById(R.id.TxtUserName);
+
+        displayQuantity(quantity);
+        calculatePrice(quantity,5);
+
+        OrderSummary = UserName.getText() +"\nWihppedCream : "+ hasWhippedCream +"\nChocolate : " + hasChecolat +
+                "\nquantity : " + quantity +
+                "\nTotal is : " + price +
+                "\nThanks You !! ";
+        priceTextView.setText(OrderSummary);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "This the Coffe Order Summary for : " + UserName.getText());
+        intent.putExtra(Intent.EXTRA_TEXT,OrderSummary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+        priceTextView.setText(OrderSummary);
     }
 
     /**
@@ -86,14 +125,34 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void increment(View view) {
+    public void increment(View view){
 
+        if (quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity = quantity + 1;
         display(quantity);
+        displayQuantity(quantity);
+
+
     }
 
     public void decrement(View view) {
 
         quantity =quantity - 1;
         display(quantity);
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+
+
     }
 }
